4cad7c45dc448208cc75512a1089b83b0d81ff4a
davodhiambo <jesseomolo@yahoo.com>
2024-08-11 18:04:42 +0300

    feat: add AppendToDisplay function to index.js.

---
+++ web/static/js/index.js
@@ -0,0 +1,15 @@
+// Calculator integration
+
+const display = document.getElementById("display");
+
+function AppendToDisplay(input) {
+    display.value += input;
+}
+
+function Calculate(input) {
+
+}
+
+function ClearDisplay() {
+
+}
